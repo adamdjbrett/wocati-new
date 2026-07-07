853d28aa0ea83b50f98ee502df130531105053db
adamdjbrett <adamdjbrett@users.noreply.github.com>
2026-07-07 14:09:55 -0500

    11ty upgrade

--- src/assets/js/_main.js
+++ src/assets/js/_main.js
@@ -12,26 +12,6 @@ $(document).ready(function () {
     $(".author__urls-wrapper").find("button").toggleClass("open");
   });
 
-  // Close search screen with Esc key
-  $(document).keyup(function (e) {
-    if (e.keyCode === 27) {
-      if ($(".initial-content").hasClass("is--hidden")) {
-        $(".search-content").toggleClass("is--visible");
-        $(".initial-content").toggleClass("is--hidden");
-      }
-    }
-  });
-
-  // Search toggle
-  $(".search__toggle").on("click", function () {
-    $(".search-content").toggleClass("is--visible");
-    $(".initial-content").toggleClass("is--hidden");
-    // set focus on input
-    setTimeout(function () {
-      $(".search-content input").focus();
-    }, 400);
-  });
-
   // Smooth scrolling
   var scroll = new SmoothScroll('a[href*="#"]', {
     offset: 20,
